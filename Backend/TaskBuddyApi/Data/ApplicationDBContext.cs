8aa1f6861b252483e5bc2f62f475fe278c686477
Mayur Gadakh <mayurgadakh07@gmail.com>
2025-01-30 16:03:29 +0530

    DB connection Done

---
+++ Backend/TaskBuddyApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskBuddyApi.Model;
+
+namespace TaskBuddyApi.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { 
+        }
+
+        public DbSet<Tasker> Taskers { get; set; }
+    }
+}
---
+++ Backend/TaskBuddyApi/Model/Tasker.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TaskBuddyApi.Model
+{
+    public class Tasker
+    {
+
+
+            [Key] // Indicates that 'Id' is the primary key
+            public int Id { get; set; }
+
+            [Required] // Marks 'Name' as a required field
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress] // Validates the email format
+            public string Email { get; set; }
+
+            [Required]
+            public string Password { get; set; }
+
+            public string Phone { get; set; }
+            public string Address { get; set; }
+            public string Category { get; set; }
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        }
+    }
+
+
+
