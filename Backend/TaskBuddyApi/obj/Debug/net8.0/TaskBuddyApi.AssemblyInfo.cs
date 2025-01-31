3bd66e1c218deaea5531a1c15c2b309f40c1a19a
Abhi-siddheshware <siddheshwareabhay@gmail.com>
2025-01-31 12:37:51 +0530

    Login,aboutUs and contact us page done

--- Backend/TaskBuddyApi/obj/Debug/net8.0/TaskBuddyApi.AssemblyInfo.cs
+++ Backend/TaskBuddyApi/obj/Debug/net8.0/TaskBuddyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa1f6861b252483e5bc2f62f475fe278c686477")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86074361e935d7c3d9cc75dff4fe49acc2296b7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
