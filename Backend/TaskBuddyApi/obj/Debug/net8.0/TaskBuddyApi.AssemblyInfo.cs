1b2193c0f3d2cc61886f42ab5929506f5fb6e190
Abhishek Pawar <pawarabhishek003@gmail.com>
2025-02-18 16:52:36 +0530

    admin controller

--- Backend/TaskBuddyApi/obj/Debug/net8.0/TaskBuddyApi.AssemblyInfo.cs
+++ Backend/TaskBuddyApi/obj/Debug/net8.0/TaskBuddyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8368e4bbbff348aaf5191032513ad0d28964cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34458beb5cbc385698a09b2afdef0dd2a9c6dbbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskBuddyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
